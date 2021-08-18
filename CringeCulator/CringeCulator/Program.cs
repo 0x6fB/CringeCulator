8cba4c52248e300255f27a97c5f17d191a785824
root <56678485+0x6fB@users.noreply.github.com>
2021-08-18 18:59:33 +0500

    Update Program.cs

--- CringeCulator/CringeCulator/Program.cs
+++ CringeCulator/CringeCulator/Program.cs
@@ -17,9 +17,7 @@ namespace CringeCulator
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //*
-            https://discord.gg/pRwbE7Y8hG
-            *//
+            //https://discord.gg/pRwbE7Y8hG
         }
     }
 }
